dc7f210d952e75237a4af109754f55b68de474c9
syaring <sarah.k.serim@gmail.com>
2018-09-27 21:12:27 +0900

    graph ql tutorial 24 book list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import BookList from './Components/BookList';
 
 class App extends Component {
   render() {
     return (
       <div id="main">
         <h1>Serim's Reading List</h1>
-
+        <BookList />
       </div>
     );
   }
