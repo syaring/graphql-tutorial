2a47164eb61e4be87ccb29f4b5a95578e885956e
syaring <sarah.k.serim@gmail.com>
2018-09-27 22:43:17 +0900

    graph ql tutorial 25 apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import BookList from './Components/BookList';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Serim's Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Serim's Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
