09fed6636f4be45e29d52e173739464ab71a600e
syaring <sarah.k.serim@gmail.com>
2018-09-27 18:21:08 +0900

    graph ql tutorial 18 mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -42,6 +42,26 @@ const BookType = new GraphQLObjectType({
   })
 });
 
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args:{
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        return author.save();
+      }
+    }
+  }
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -75,6 +95,6 @@ const RootQuery = new GraphQLObjectType({
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
-
