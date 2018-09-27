f02a5d1cd188ad7ac648f25973e49e8e728e0074
syaring <sarah.k.serim@gmail.com>
2018-09-27 20:54:00 +0900

    graph ql tutorial 23 add front(react)

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Serim's Reading List</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
